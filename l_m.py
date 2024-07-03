fe86e5a1d3eb69ead332338bc60e3d50e3863ecc
SharnuSharath123 <sharnuburli143@gmail.com>
2024-07-03 23:08:44 +0530

    pack

---
+++ l_m.py
@@ -0,0 +1,26 @@
+# approach 1
+import l
+import m
+
+l.fly()
+l.color()
+l.behaviour()
+l.creative()
+
+m.fly()
+m.color()
+m.behaviour()
+m.creative()
+
+# approach 2
+from l import *
+fly()
+color()
+behaviour()
+creative()
+
+from m import *
+fly()
+color()
+behaviour()
+creative()
