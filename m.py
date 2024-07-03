e947fdaa389b545fde01ed70849648c780ca844a
SharnuSharath123 <sharnuburli143@gmail.com>
2024-07-03 23:08:21 +0530

    pack

---
+++ m.py
@@ -0,0 +1,11 @@
+def fly():
+    print("Bird can't fly")
+
+def color():
+    print("Bird is blue")
+
+def behaviour():
+    print("Bird behaviour is sparking")
+
+def creative():
+    print("Bird looks very nice!!!")
